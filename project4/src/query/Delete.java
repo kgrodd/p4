7c25902ca15bf9f05f3c21a6f081049fa57d5469
kgrodd <kgrodd@purdue.edu>
2016-04-13 16:38:31 -0400

    required

--- project4/src/query/Delete.java
+++ project4/src/query/Delete.java
@@ -1,11 +1,26 @@
 package query;
 
 import parser.AST_Delete;
+import parser.ParseException;
+import relop.Schema;
+import global.Minibase;
+import heap.HeapFile;
+import heap.HeapScan;
+import global.RID;
+import global.SearchKey;
+import relop.Tuple;
+import relop.Predicate;
+import query.IndexDesc;
+import index.HashIndex;
+
 
 /**
  * Execution plan for deleting tuples.
  */
 class Delete implements Plan {
+  protected String tableName;
+	protected Predicate[][] preds;
+  protected Schema schema;
 
   /**
    * Optimizes the plan, given the parsed query.
@@ -13,6 +28,10 @@ class Delete implements Plan {
    * @throws QueryException if table doesn't exist or predicates are invalid
    */
   public Delete(AST_Delete tree) throws QueryException {
+		tableName = tree.getFileName();
+		preds = tree.getPredicates();  	
+		schema = QueryCheck.tableExists(tableName);
+		QueryCheck.predicates(schema, preds);
 
   } // public Delete(AST_Delete tree) throws QueryException
 
